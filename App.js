77dad5b7913500f922559811d84ca542ca1faba0
Khoa Truong <khoatruong@Khoas-iMac.local>
2024-03-10 21:49:01 +0200

    configuring live chat app

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import {   View } from 'react-native';
+import { styles } from './style/AppStyle';
+// firebase SDK 
+// import firebase from 'firebase/app'
+import firebase from 'firebase/compat/app';
+
+import 'firebase/compat/auth'
+
+
+// firebase hooks
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+
+
+const auth = firebase.auth()
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
